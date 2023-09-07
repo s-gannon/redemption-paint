53b2284e5e7cbbf84523b5b7073495d01fd0e318
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-06 20:41:30 -0500

    Added resources folder for glade templates

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <gtk/gtk.h>
 
-int main(void){
-	printf("Hello world!\n");
+const char GLADE_LAYOUT[] = "./../res/glade_template.glade";
+
+int main(int argc, char** argv){
+	gtk_init(&argc, &argv);
+	
+	GtkBuilder *builder = gtk_builder_new_from_file(GLADE_LAYOUT);
+	
+	GtkWidget *window = (GtkWidget *) gtk_builder_get_object(
+		builder, 
+		"window1"
+	);
+
+	gtk_widget_show_all(window);
+	gtk_main();
+
 	return 0;
 }
