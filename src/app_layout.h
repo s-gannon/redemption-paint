fbddcd912de3d514f82ecc44d253dc0e4d99adc0
Spencer Gannon <spencer.gannon@valpo.edu>
2023-09-08 14:41:05 -0500

    Removing glade files, rewriting layout to app_layout.h

---
+++ src/app_layout.h
@@ -0,0 +1,12 @@
+#ifndef APP_LAYOUT_H
+#define APP_LAYOUT_H
+
+
+
+#endif
+#ifndef APP_LAYOUT_SRC
+#define APP_LAYOUT_SRC
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include <limits.h>
 #include <gtk/gtk.h>
 
-
 typedef struct image_t{
-	GtkWidget* image;
+	GtkWidget* gtk_image;
+	GtkWidget** parents;
 	char file_path[PATH_MAX];
 } image_t;
 /*
@@ -22,7 +22,9 @@ void quit_program(GtkWidget* widget, gpointer ptr){
 }
 
 void file_open(GtkWidget* widget, gpointer ptr){
+	assert(ptr != NULL);
 	image_t* img = ptr;
+	assert((img->gtk_image != NULL) && (img->parents != NULL));
 	GtkWidget *dialog = gtk_file_chooser_dialog_new(
 		"Open file", 
 		GTK_WINDOW(NULL),
@@ -40,87 +42,33 @@ void file_open(GtkWidget* widget, gpointer ptr){
 			),
 			PATH_MAX
 		);
+		assert(img->file_path != NULL);
 		g_print("Image file path set to %s\n", img->file_path);
-		img->image = gtk_image_new_from_file(img->file_path);
+		img->gtk_image = gtk_image_new_from_file(img->file_path);
 	}
 
 	gtk_widget_destroy(dialog);
-}
-
-void file_save_as(GtkWidget* widget, gpointer ptr){
-	image_t* img = ptr;
-
-	GtkWidget *dialog = gtk_file_chooser_dialog_new(
-		"Open file", 
-		GTK_WINDOW(NULL),
-		GTK_FILE_CHOOSER_ACTION_SAVE,
-		"Cancel", 0,
-		"OK", 1,
-		NULL
-	);
-	
-	if(gtk_dialog_run(GTK_DIALOG(dialog))){
-		strncpy(
-			img->file_path, 
-			gtk_file_chooser_get_filename(
-				GTK_FILE_CHOOSER(dialog)
-			),
-			PATH_MAX
-		);
-		g_print("Image file saved to %s\n", img->file_path);
-	}
-
-	gtk_widget_destroy(dialog);
-}
-
-void file_save(GtkWidget* widget, gpointer ptr){
-	image_t* img = ptr;
-
-	if(img->file_path == NULL){
-		file_save_as(widget, ptr);
-		return 0;
-	}
-
-	//to-do: saving image
 }
 
 int main(int argc, char** argv){
 	image_t* image = malloc(sizeof(image_t));
+	
 	// fuck you realpath
 	char* glade_layout_path = realpath(GLADE_LAYOUT_RELATIVE_PATH, NULL);
 
 	gtk_init(&argc, &argv);
 
-	if(!glade_layout_path){	//if glade_layout_path == NULL
-		perror("Loading glade layout file!");
-		printf(
-			"%s\n%s\n%d\n", 
-			glade_layout_path, 
-			GLADE_LAYOUT_RELATIVE_PATH, 
-			errno
-		);
-		return errno;
-	}
 	// GTK Widgets
-	GtkBuilder *builder = gtk_builder_new_from_file(glade_layout_path);
-	GtkWidget *window = (GtkWidget *) gtk_builder_get_object(
-		builder, 
-		"main_window"
-	);
-	GtkWidget *file_quit = (GtkWidget *) gtk_builder_get_object(
-		builder, 
-		"file_quit"
-	);
-	GtkWidget *file_open = (GtkWidget *) gtk_builder_get_object(
-		builder, 
-		"file_open"
-	);
+	GtkBuilder *builder   = gtk_builder_new_from_file(glade_layout_path);
+	assert(builder != NULL);
+	GtkWidget  *window    = (GtkWidget *) gtk_builder_get_object(builder, "main_window");
+	GtkWidget  *file_quit = (GtkWidget *) gtk_builder_get_object(builder, "file_quit"  );
+	GtkWidget  *file_open = (GtkWidget *) gtk_builder_get_object(builder, "file_open"  );
 
 	// GTK Signals
-	g_signal_connect(window, "delete_event", G_CALLBACK(quit_program), NULL);
-	g_signal_connect(file_quit, "activate", G_CALLBACK(quit_program), NULL);
-	g_signal_connect(file_open, "activate", G_CALLBACK(file_open), image);
-	g_signal_connect(file_save, "activate", G_CALLBACK(file_save), image);
+	g_signal_connect(window   , "delete_event", G_CALLBACK(quit_program), NULL );
+	g_signal_connect(file_quit, "activate"    , G_CALLBACK(quit_program), NULL );
+	g_signal_connect(file_open, "activate"    , G_CALLBACK(file_open)   , image);
 
 	// Showtime and start GTK
 	gtk_widget_show_all(window);
